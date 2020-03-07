e47009fcbac4d23b121cc86cf14d69053be8b72e
Austin Redmond <aredmond85@gmail.com>
2020-03-07 17:24:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,8 @@ def line (katz_deli)
   end
 end
 
-def take_a_number(katz_deli, number)
 
+def take_a_number(katz_deli, customer)
+  katz_deli << customer
+  puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
 end
