530afac747c1d9e5638ac5475800c9de44fcd77f
Austin Redmond <aredmond85@gmail.com>
2020-03-07 18:12:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,5 +24,9 @@ end
 
 
 def now_serving(katz_deli)
-  
+  if katz_deli.length < 1
+    puts "There is nobody waiting to be served!"
+  else
+    "Currently serving #{katz_deli.shift()}"
+  end
 end
