ab817827debf5a23d06cfe7e8b8c7f66d55d73fc
Austin Redmond <aredmond85@gmail.com>
2020-03-07 17:11:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+# Deli is EMPTY or EMPTY arrays
+katz_deli = []
+# OUTPUTS current place in line. "The line is currently: #(number). #{customer}"
+# IF nobody is line OUTPUTS "The line is currently empty."
+def line (katz_deli)
+  line_position = []
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    number = 1
+    katz_deli.each do |customer|
+      line_position << "#{number}. #{customer}"
+    number += 1
+    end
+    puts "The line is currently: " + line_position.join(' ')
+  end
+end
+
+def take_a_number(katz_deli, number)
+
+end
